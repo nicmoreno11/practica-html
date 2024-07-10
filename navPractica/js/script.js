173f903150d8caaceb3272b2da8f0adae3ec7498
nicmoreno11 <nicmoreno11@misena.edu.co>
2024-07-10 12:40:00 -0500

    practica-html

---
+++ navPractica/js/script.js
@@ -0,0 +1,8 @@
+/*window.onSpotifyIframeApiReady = (IFrameAPI) =>{
+    const element = document.getElementById('frame');
+    const options = {
+        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+    };
+    const callback = (EmbedController) =>{};
+    IFrameAPI.createController(element, options, callback)
+};*/
